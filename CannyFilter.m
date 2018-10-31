757d47e79c98aabe4af9f129e3834eea517762ed
Sabrina Oblasser <e11709474@student.tuwien.ac.at>
2018-10-31 18:30:16 +0100

    Canny Filter
    Einlesen des Bildes, Graustufenbild erzeugen und Canny Filter hinzugefügt

--- CannyFilter.m
+++ CannyFilter.m
@@ -1,1 +1,15 @@
-%Test
+
+bildPfad='C:\TU\Smileyerkennung\Testbild.jpg';
+dateityp='jpg';
+
+%bild einlesen
+bild=imread(bildPfad,dateityp);
+imshow(bild);
+
+%bild in Graustufenbild umwandeln
+grayskaleImage=rgb2gray(bild);
+imshow(grayskaleImage);
+
+%Canny Filter
+canny=edge(grayskaleImage,'canny',[],3);
+imshow(canny);
