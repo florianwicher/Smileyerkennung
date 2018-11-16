eb3713a7d0dbd4518ebe68f40aa884b4e0aa2a69
Lippeck <daniel.lippeck@outlook.de>
2018-11-16 16:56:04 +0100

    Minor changes

--- EyeDetetion.m
+++ EyeDetetion.m
@@ -1,35 +1,40 @@
-function Picture = EyeDetetion(Img)
+ function Picture = EyeDetetion(Img)
 
-Img=imbinarize(Img(:,:,3)); 
+Picture = Img;
+[w,e,r] = size(Picture);
+Img=imbinarize(Img(:,:,3));
 Rmin=20;
 Rmax=50;  
 [centersDark, radiiDark] = imfindcircles(Img, [Rmin Rmax],'ObjectPolarity','dark','sensitivity',0.90);
 
-Glass = imread('Glasses1.jpg');
-Glass = imbinarize(Glass(:,:,3));
-
-Glass = ~Glass;
-
+%Abstand zwischen Augen ausrechnen
 dX = centersDark(1,1) - centersDark(1,2);
 dY = centersDark(2,1) - centersDark(2,2);
 H = sqrt(dX^2+dY^2);
-alpha = asind(dX/H);
-Glass = imrotate(Glass, -alpha,'bilinear','crop'); 
 
-Glass = ~Glass;
+Glass = imread('Glasses.jpg');
+%TODO Glasses Größe je nach Augenabstand ändern
 
-oX = -20;
-oY = 0;
+%TODO Glasses drehen je nach Augenwinkel
 
-[n,m] = size(Glass);
+%Länge und Breite Abspeichern
+[n,m,o] = size(Glass);
+%TODO Glasses Startpunkt berechnen je nach Winkel
+oX = round(centersDark(1,1))-round(n/2.9);
+oY = round(centersDark(1,2))-round(m/2.95);
 
- for y = 1:m 
-     for x = 1:n    
-         if Glass(x,y) < 1
-             Img(x+oX,y+oY) = 0;
+%Glasses auf Smiley überschreiben
+ for x = 1:n 
+     for y = 1:m  
+         for z = 1:o
+            if Glass(x,y,z) < 100
+                if(x+oX)>0 && (x+oX)<w && (y+oY)>0 && (y+oY)<e
+                    Picture(x+oX,y+oY,z) = Glass(x,y,z);
+                end
+            end
          end
      end
  end
  
- Picture = Img;
+
  
