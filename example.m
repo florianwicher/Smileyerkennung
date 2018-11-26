9d9f617daf4ae039bcbcf960189ab8766d5f527d
Lippeck <daniel.lippeck@outlook.de>
2018-11-26 14:14:01 +0100

    Example updatefd

--- example.m
+++ example.m
@@ -1,11 +1,11 @@
-I = imread('Datensatz/IMG_20181117_155407.jpg');
+I = imread('Datensatz/IMG_20181117_155415.jpg');
+I2 = imread('Datensatz/IMG_20181117_155415.jpg');
 imshow(CannyFilter(imbinarize(rgb2gray(I)),0.1));
 
 [x0,y0,a,b,alpha] = FindBestEllipse(I);
 
 figure;
 image(I);
-
 ellipse_draw(a,b,alpha*pi/180,x0,y0,'r');
 
 
@@ -14,5 +14,7 @@ img = imgTransform(I, T);
 figure;
 imshow(img);
 
-figure;
-imshow(imgTransform(img, inverse));
+img = imgTransform(img, inverse);
+
+img2 = EyeDetection(img, 800, 1100, 500);
+imshow(img2);
