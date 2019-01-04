1d86c241493d6e44b1de7077ed7e54ed60664b06
julieklnfrchnr <july.kleinferchner@gmail.com>
2019-01-04 22:50:00 +0100

    FindBestEllipse added median filtering and opening and edge limitation

--- FindBestEllipse.m
+++ FindBestEllipse.m
@@ -2,11 +2,11 @@ function [x0,y0,a,b,alpha] = FindBestEllipse(image)
 
 F = FindSheet(image);
 
-    params.minMajorAxis = 0;
+    params.minMajorAxis = size(image,2)/50;
     params.maxMajorAxis = 1500;
     params.rotation = 0;
     params.rotationSpan = 0;
-    params.minAspectRatio = 0.2;
+    params.minAspectRatio = 0.5;
     params.randomize = 10;
     params.numBest =  10;
     params.uniformWeights = true;
@@ -17,11 +17,16 @@ F = FindSheet(image);
 for i = 1:size(F,1)
     
     croppedImage = imcrop(image, F(i,1:4));
-    kantenbild = CannyFilter(imbinarize(rgb2gray(croppedImage)),0.2);
-    values = ellipseDetection(kantenbild, params);
-    values(:,1)= values(:,1)+F(i,1);
-    values(:,2)= values(:,2)+F(i,2);
-    bestFits = cat(1, bestFits, values);
+    binaryImage = imbinarize(rgb2gray(croppedImage));
+    binaryImage = MedianFilter(binaryImage);
+    binaryImage = OpenImage(binaryImage,5);
+    kantenbild = CannyFilter(binaryImage,0.2);
+    if sum(kantenbild(:)) < 10000000
+        values = ellipseDetection(kantenbild, params);
+        values(:,1)= values(:,1)+F(i,1);
+        values(:,2)= values(:,2)+F(i,2);
+        bestFits = cat(1, bestFits, values);
+    end
     
 end
 
--- MedianFilter.m
+++ MedianFilter.m
@@ -5,11 +5,11 @@ function [filtered_image] = MedianFilter(img)
     
     %go through every pixel in the image, ignore the border
     for i = 2:(size(img,1)-1)
-        for j = 1:(size(img,2)-1)
+        for j = 2:(size(img,2)-1)
             %check a 3x3 neighborhood around the pixel
-            neighborhood = img((j-1):(j+1),(i-1):(i+1));
+            neighborhood = img((i-1):(i+1),(j-1):(j+1));
             %pixel at position j,i is the median of the neighborhood
-            filtered_image(j,i) = median(neighborhood(:));
+            filtered_image(i,j) = median(neighborhood(:));
         end
     end
 end
