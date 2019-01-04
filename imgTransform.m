d64d8d78099ab5ee6558a9fe56305a432967e0c6
Mazi-S <mathias.schwengerer@aon.at>
2019-01-05 00:43:33 +0100

    Transformation

--- imgTransform.m
+++ imgTransform.m
@@ -1,3 +1,16 @@
 function B=imgTransform(A, T)
-% Transformiert das Bild A mit der Transformationsmatrix T
-B = imwarp(A, affine2d(T));
+
+s = [size(A,2), size(A,1), 1] * T;
+
+B = zeros(round(s(2)), round(s(1)), 3,'uint8');
+
+invT = inv(T);
+Y = round( [1:size(B,1)] .* invT(2,1) + [1:size(B,1)] .* invT(2,2) + [1:size(B,1)] .* invT(2,3) ); 
+X = round( [1:size(B,2)] .* invT(1,1) + [1:size(B,2)] .* invT(1,2) + [1:size(B,2)] .* invT(1,3) ); 
+       
+y = 1:size(B,1);
+x = 1:size(B,2);
+
+B(y, x, 1) = A(Y(y), X(x), 1);
+B(y, x, 2) = A(Y(y), X(x), 2);
+B(y, x, 3) = A(Y(y), X(x), 3);
